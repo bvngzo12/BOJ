c2338f51bb6424d6cb29c0a90fa21b64c819b323
bvngzo12 <33623151+bvngzo12@users.noreply.github.com>
2024-08-27 15:17:09 +0900

    [Gold III] Title: 감시, Time: 172 ms, Memory: 20840 KB -BaekjoonHub

---
+++ 백준/Gold/15683. 감시/감시.java
@@ -0,0 +1,113 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int R, C, blind;
+	static int ans = Integer.MAX_VALUE;
+	static int[][] M;
+	static ArrayList<int[]> cctv;
+	
+	static int[] dr = {0,-1,0,1};
+	static int[] dc = {1,0,-1,0};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		M = new int[R][C];
+		
+		cctv = new ArrayList();
+		
+		
+		// 자료 입력 + cctv 좌표 수집
+		for(int i = 0; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j = 0; j < C; j++) {
+				M[i][j] = Integer.parseInt(st.nextToken());
+				if(M[i][j]>=1 && M[i][j]<=5) {
+					cctv.add(new int[] {i,j});
+				}
+				else if(M[i][j] == 0) blind++;
+			}
+		}
+		
+		cctvOn(0);
+		System.out.println(ans);
+	}
+	
+	static void cctvOn(int idx) {
+		if(idx == cctv.size()) {
+			ans = Math.min(ans, blind);
+			return;
+		}
+		
+		int[] temp = cctv.get(idx);
+		int type = M[temp[0]][temp[1]];
+		
+		for(int d = 0; d < dr.length; d++) {	
+			monitorOn(idx,d,type,-(idx+1));		// 마킹
+			cctvOn(idx+1);
+			monitorOn(idx,d,type,0);			// 언마킹
+		}
+	}
+	
+	static void monitorOn(int i, int d, int type, int value) {
+		int[] curr = cctv.get(i);
+		int r = curr[0];
+		int c = curr[1];
+		int id = -(i+1);
+		switch(type) {
+		case 1:
+			marking(r,c,d,value,id);
+			return;
+			
+		case 2:
+			marking(r,c,d,value,id);
+			marking(r,c,(d+2)%4,value,id);
+			return;
+		case 3:
+			marking(r,c,d,value,id);
+			marking(r,c,(d+1)%4,value,id);
+			return;
+		case 4:
+			marking(r,c,d,value,id);
+			marking(r,c,(d+1)%4,value,id);
+			marking(r,c,(d+2)%4,value,id);
+			return;
+		case 5:
+			marking(r,c,d,value,id);
+			marking(r,c,(d+1)%4,value,id);
+			marking(r,c,(d+2)%4,value,id);
+			marking(r,c,(d+3)%4,value,id);
+			return;
+		}
+	}
+	
+	static void marking(int r, int c, int d, int val, int id) {
+		int nr = r+dr[d];
+		int nc = c+dc[d];
+		if(nr < 0 || nr >= R|| nc < 0 || nc >= C || M[nr][nc] == 6)return;
+		
+		if(val == 0 && M[nr][nc] == id) {		// 언마킹
+			M[nr][nc] = 0;
+			blind++;
+		}
+		else if(val != 0 && M[nr][nc] == 0) {						// 마킹
+			M[nr][nc] = val;
+			blind--;
+		}
+		
+		marking(nr,nc,d,val,id);
+		
+	}
+	
+
+}
