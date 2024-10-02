b874079c755834a313d9c30ed3415113bea6ad09
bvngzo12 <33623151+bvngzo12@users.noreply.github.com>
2024-10-02 12:24:26 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 131 ms, Memory: 19,464 KB -BaekjoonHub

--- SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -1,69 +1,121 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
+/*
+하나의 톱니바퀴는 8개 톱니 (N, S)를 가짐 => 4개 => [4][8][2]
+K번 회전 = 방향 2개
+
+맞닿은 톱니의 극이 다르면, 반대로
+같으면 회전 안 함
+ * */
 
 public class Solution {
+	static int[][] gear = new int[4][8];
+	static int T, K;
 
-	static String[] wheels = new String[4];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
+		T = Integer.parseInt(br.readLine());
 		
-		for(int t = 1; t <= T; t++) {
-			
-			int N = sc.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			K = Integer.parseInt(br.readLine().split(" ")[0]);
 			
 			for (int i = 0; i < 4; i++) {
-				String w = "";
-				for(int j = 0; j < 8; j++) {
-					w += sc.nextInt();		
-				}
-				wheels[i] = w;
-			}
-
-			for (int i = 0; i < N; i++) {
-				int idx = sc.nextInt()-1;
-				int cmd = sc.nextInt();
-
-				rotate(idx, cmd);
-				chainEffect(idx, idx + 1, cmd);
-				chainEffect(idx, idx - 1, cmd);
-			}
-
-			int score = 0;
-
-			for (int n = 0; n < 4; n++) {
-				if (wheels[n].charAt(0) == '1') {
-					score += Math.pow(2, n);
+				String[] input = br.readLine().split(" ");
+				for (int j = 0; j < 8; j++) {
+					gear[i][j] = Integer.parseInt(input[j]);
 				}
 			}
 
-			System.out.printf("#%d %d\n",t,score);
-		}
+			for (int i = 0; i < K; i++) {
+				String[] input2 = br.readLine().split(" ");
+				int no = Integer.parseInt(input2[0]);
+				int dir = Integer.parseInt(input2[1]);
 
+				// 번호와 방향을 받아 돌리기 시작
+				round(no - 1, dir);
+			}
+
+			int answer = 0;
+			int add = 1;
+			for (int i = 0; i < 4; i++) {
+				if (gear[i][0] == 1) {
+					answer += add;
+				}
+				add *= 2;
+			}
+
+			bw.write("#" + tc + " " + answer + "\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
-	static void rotate(int idx, int opt) {
-		String str = wheels[idx];
-		if (opt == 1) {
-			wheels[idx] = str.charAt(str.length() - 1) + str.substring(0, str.length() - 1);
-		} else if (opt == -1) {
-			wheels[idx] = str.substring(1, str.length()) + str.charAt(0);
+	public static void round(int no, int dir) {
+		// 인접한 바퀴 찾기
+		// 1이면 2의 6 / 2이면 1의 2와 3의 6 / 3이면 2의 2와 4의 6 / 4면 3의 2
+		// 값이 다르면 큐에 넣기, 아니면 패스
+		Queue<int[]> queue = new ArrayDeque<>();
+		queue.offer(new int[] { no, dir });
+		
+		int opDir = 0;
+		if (dir == 1) {
+			opDir = -1;
+		} else {
+			opDir = 1;
 		}
-	}
-
-	static void chainEffect(int self, int idx, int cmd) {
-		if (idx < 0 || idx >= 4) {
-			return;
+		
+		int leftNum = no;
+		int rightNum = no;
+		int leftOpDir = opDir;
+		int rightOpDir = opDir;
+		
+		while (leftNum != -1 && rightNum != 4) {
+			boolean change = false;
+			
+			if (leftNum - 1 != -1 && gear[leftNum][6] != gear[leftNum - 1][2]) {
+				queue.offer(new int[] { leftNum - 1, leftOpDir });
+				leftNum = leftNum-1;
+				leftOpDir = -leftOpDir;
+				change = true;
+			} else if (rightNum + 1 != 4 && gear[rightNum][2] != gear[rightNum + 1][6]) {
+				queue.offer(new int[] { rightNum + 1, rightOpDir });
+				rightNum = rightNum+1;
+				rightOpDir = -rightOpDir;
+				change = true;
+			}
+			
+			if (!change) break; 
 		}
-		// 동시에 회전 하기대문에 회전 self는 이전 인덱스 값으로 비교 
-		if (self < idx && wheels[self].charAt(2+cmd) != wheels[idx].charAt(6)) {	
-			rotate(idx, cmd * (-1));
-			chainEffect(idx, idx + 1, cmd * (-1));
+		
+		// 하나씩 빼면서 gear 회전 업데이트
+		while (queue.size() != 0) {
+			int[] cur = queue.poll();
+			
+			// 시계방향 오른쪽, gear[0] = gear[7]
+			if (cur[1] == 1) {
+				int temp = gear[cur[0]][7];
 
-		} else if (self > idx && wheels[self].charAt(6+cmd) != wheels[idx].charAt(2)) {
-			rotate(idx, cmd * (-1));
-			chainEffect(idx, idx - 1, cmd * (-1));
+				for (int i = 7; i >= 1; i--) {
+					gear[cur[0]][i] = gear[cur[0]][i-1];
+				}
+
+				gear[cur[0]][0] = temp;
+			}
+			// 반시계방향 왼쪽, gear[7] = gear[0]
+			else {
+				int temp = gear[cur[0]][0];
+
+				for (int i = 1; i < 8; i++) {
+					gear[cur[0]][i - 1] = gear[cur[0]][i];
+				}
+
+				gear[cur[0]][7] = temp;
+			}
 		}
 	}
 }
