97dad551a1f51d651b9d08b562c868b322972f53
bvngzo12 <33623151+bvngzo12@users.noreply.github.com>
2024-10-08 09:13:22 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11564 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,27 +1,33 @@
-
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
+	static int T, num;
+	static int [] mem;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int T = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		for(int t = 0; t < T; t++) {
-			int N = sc.nextInt();
-			int[] dp = new int[11];
+		T = Integer.parseInt(br.readLine().split(" ")[0]);
+		
+		for (int tc = 1; tc <= T; tc++) {
+			num = Integer.parseInt(br.readLine().split(" ")[0]);
 			
-			dp[1] = 1;
-			dp[2] = 2;
-			dp[3] = 4;
+			mem = new int[15];
+			mem[1] = 1;
+			mem[2] = 2;
+			mem[3] = 4;
 			
-			for(int i = 4; i <= N; i++) {
-				dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+			for (int i = 4; i <= num; i++) {
+				mem[i] = mem[i-3] + mem[i-2] + mem[i-1];
 			}
 			
-			System.out.println(dp[N]);
+			bw.write(mem[num] + "\n");
 		}
+		
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 
 }
