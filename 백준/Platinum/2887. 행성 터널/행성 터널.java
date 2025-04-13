f4997b1b2f11e184a39e7e72b46e30448f4a5aa4
bvngzo12 <33623151+bvngzo12@users.noreply.github.com>
2025-04-13 23:05:52 +0900

    [Platinum V] Title: 행성 터널, Time: 1812 ms, Memory: 101476 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.java
+++ 백준/Platinum/2887. 행성 터널/행성 터널.java
@@ -1,118 +1,103 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, answer;
-	static ArrayList<int[]> xList, yList, zList;
-	static List<Planet> edgeList;
-	static int[] parent;
+
+	static class Vertex implements Comparable<Vertex>{
+
+		int e;
+		int dist;
+		
+		Vertex(int e, int w){
+			this.e = e;
+			this.dist = w;
+		}
+		
+		@Override
+		public int compareTo(Vertex o) {
+			return this.dist - o.dist;
+		}
+		
+	}
 	
-	static class Planet {
-		int from, to, cost;
+	public static void main(String[] args) throws IOException{
 		
-		Planet(int from, int to, int cost) {
-			this.from = from;
-			this.to = to;
-			this.cost = cost;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		N = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 		
-		// x, y, z 따로 관리
-		//cost 계산이 x, y, z 중 최솟값이므로 따로 리스트를 만들고 각 좌표끼리 거리 계산
-		xList = new ArrayList<>();
-		yList = new ArrayList<>();
-		zList = new ArrayList<>();	
-		edgeList = new ArrayList<>();
-		for (int i = 0; i < N; i++) {
+		int[][] points = new int[n][3];
+		
+		List<Vertex>[] graph = new ArrayList[n];
+		
+		for(int i = 0; i < n; i++) {
+			graph[i] = new ArrayList<Vertex>();
+			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-			int z = Integer.parseInt(st.nextToken());
-			
-			xList.add(new int[] {i, x});
-			yList.add(new int[] {i, y});
-			zList.add(new int[] {i, z});
+			points[i] = new int[] {i, Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
 		}
-
-		//오름차순 정렬
-		Collections.sort(xList, (p1, p2) -> p1[1] - p2[1]);
-		Collections.sort(yList, (p1, p2) -> p1[1] - p2[1]);
-		Collections.sort(zList, (p1, p2) -> p1[1] - p2[1]);
 		
-		//인접해있는 행성들끼리 cost 계산
-		calCost(xList);
-		calCost(yList);
-		calCost(zList);
-		
-		//간선리스트 정렬
-		Collections.sort(edgeList, (p1, p2) -> p1.cost - p2.cost);
-		
-		//크루스칼
-		make();
-		
-		run();
-		
-		bw.write(answer + "\n");
-		bw.close();
-		br.close();
-
-	}
-
-	private static void run() {
-		int edgeNum = 0;
-		for (int i = 0; i < edgeList.size(); i++) {
-			Planet cur = edgeList.get(i);
+		for(int i = 1; i < 4; i++) {
 			
-			if (union(cur.from, cur.to)) {
-				answer += cur.cost;
-				edgeNum++;
-				if (edgeNum == N-1) break;
+			int index = i;
+			Arrays.sort(points, (p1, p2) -> Integer.compare(p1[index], p2[index]));
+			
+			for(int p = 0; p < n-1; p++) {
+				int[] before = points[p];
+				int[] nxt = points[p+1];
+				
+				int dist = getDist(before, nxt);
+				
+				graph[before[0]].add(new Vertex(nxt[0], dist));
+				graph[nxt[0]].add(new Vertex(before[0], dist));
 			}
-		}
-	}
-
-	private static boolean union(int from, int to) {
-		int parentA = find(from);
-		int parentB = find(to);
-		
-		if (parentA == parentB) return false;
-		
-		if (parentA < parentB) {
-			parent[parentB] = parentA;
-		} else {
-			parent[parentA] = parentB;
-		}
-		
-		return true;
-	}
-
-	private static int find(int a) {
-		if (a == parent[a]) return a;
-		else return parent[a] = find(parent[a]); 
-	}
-
-	private static void make() {
-		parent = new int[N];
-		for (int i = 0; i < N; i++) {
-			parent[i] = i;
-		}
-	}
-
-	private static void calCost(ArrayList<int[]> list) {
-		for (int i = 0; i < list.size() - 1; i++) {
-			int[] first = list.get(i);
-			int[] second = list.get(i+1);
 			
-			int cost = Math.abs(first[1] - second[1]);
-			
-			edgeList.add(new Planet(first[0], second[0], cost));
 		}
+		
+		
+		int[] dist = new int[n];
+		boolean[] selected = new boolean[n];
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[0] = 0;
+		
+		PriorityQueue<Vertex> pq = new PriorityQueue<Vertex>();
+		pq.offer(new Vertex(0,0));
+		
+		while(!pq.isEmpty()) {
+			
+			Vertex curr = pq.poll();
+			
+			if(selected[curr.e]) continue;
+			selected[curr.e] = true;
+			
+			for(Vertex nxt : graph[curr.e]) {
+				
+				if(!selected[nxt.e] && dist[nxt.e] > nxt.dist) {
+					dist[nxt.e] = nxt.dist;
+					pq.offer(nxt);
+				}
+				
+			}
+			
+		}
+		
+		int total = 0;
+		
+		for(int i = 0; i < n; i++) {
+			total += dist[i];
+		}
+		
+		System.out.println(total);
+	}
+
+	private static int getDist(int[] before, int[] nxt) {
+		return Math.min(Math.abs(before[1] - nxt[1]), Math.min(Math.abs(before[2] - nxt[2]), Math.abs(before[3] - nxt[3])));
 	}
 
 }
