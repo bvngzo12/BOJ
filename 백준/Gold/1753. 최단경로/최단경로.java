84f3d0746661881f6e4c53ec2eba947fcb165422
bvngzo12 <33623151+bvngzo12@users.noreply.github.com>
2025-06-11 23:10:48 +0900

    [Gold IV] Title: 최단경로, Time: 720 ms, Memory: 121604 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -6,30 +7,31 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-class Vertex{
-	int end;
-	int weight;
-	
-	Vertex(int e, int w){
-		this.end = e;
-		this.weight = w;
-	}
-}
-
 public class Main {
+	
+	static class Vertex{
+		private int e;
+		private int w;
+		
+		Vertex(int e, int w){
+			this.e = e;
+			this.w = w;
+		}
+		
+	}
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int v = Integer.parseInt(st.nextToken());
+		int vertex = Integer.parseInt(st.nextToken());
 		int edge = Integer.parseInt(st.nextToken());
-
+		
 		int start = Integer.parseInt(br.readLine());
 		
-		List<Vertex>[] graph = new ArrayList[v+1];
+		List<Vertex>[] graph = new ArrayList[vertex+1];
 		
-		for(int i = 1; i <= v; i++) {
+		for(int i = 1; i <= vertex; i++) {
 			graph[i] = new ArrayList<Vertex>();
 		}
 		
@@ -38,44 +40,47 @@ public class Main {
 			
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
 			
-			graph[s].add(new Vertex(e, w));
+			graph[s].add(new Vertex(e,v));
 			
 		}
-
-		// 다익스트라 초기 설정
-		int[] dist = new int[v+1];					// 거리
-		Arrays.fill(dist, Integer.MAX_VALUE);		// 거리를 최잿값으로 초기화
-		boolean[] selected = new boolean[v+1];		// 대표 노드여부
+		
+		int[] dist = new int[vertex+1];
+		boolean[] selected = new boolean[vertex+1];
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((o1,o2) -> Integer.compare(o1[1], o2[1]));
+		
+		pq.offer(new int[] {start,0});
 		dist[start] = 0;
 		
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-		pq.offer(new int[] {start, 0});				// 후보 노드와 시작 노드로부터의 거리
-		
 		while(!pq.isEmpty()) {
-			
 			int[] curr = pq.poll();
 			
 			if(selected[curr[0]]) continue;
-			
 			selected[curr[0]] = true;
+			
 			for(Vertex nxt : graph[curr[0]]) {
-				int nxtNode = nxt.end;
+				if(selected[nxt.e]) continue;
 				
-				if(selected[nxtNode]) continue;
-				
-				if(dist[nxtNode] > dist[curr[0]] + nxt.weight) {
-					dist[nxtNode] = dist[curr[0]] + nxt.weight;
-					pq.offer(new int[] {nxtNode, dist[nxtNode]});
+				if(dist[nxt.e] > dist[curr[0]] + nxt.w) {
+					dist[nxt.e] = dist[curr[0]] + nxt.w;
+					pq.offer(new int[] {nxt.e, dist[nxt.e]});
 				}
 				
 			}
+			
 		}
 		
-		for(int i = 1; i < dist.length; i++) {
-			System.out.println(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]);
+		
+		StringBuilder sb = new StringBuilder();
+		for(int i = 1; i <= vertex; i++) {
+			sb.append(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]).append("\n");
 		}
+		
+		System.out.println(sb.toString());
 		
 	}
 
